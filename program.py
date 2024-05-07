ef738864c180afe4396bc7bd3c7acb433eb0613b
div493 <156229009+div493@users.noreply.github.com>
2024-05-07 09:03:34 -0400

    Add files via upload

---
+++ program.py
@@ -0,0 +1,7 @@
+echo;"# pods" >> README.md
+git;init
+git;add,README.md
+git;commit -m,"first commit"
+git;branch -M=main
+git'remote add origin https://github.com/div493/pods.git
+git;push -u origin main
